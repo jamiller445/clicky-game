93b2183fb830dae5a7940e9c2a7d82a342fe5f8d
jeffrey.miller445 <jeffrey.miller445@gmail.com>
2019-03-20 13:25:01 -0400

    Updates

---
+++ precache-manifest.ece2a91c804e70e1b1e3c6bdb349ef01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "9137603c628606c954e9",
+    "url": "/clicky-game/static/js/main.0553424f.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/clicky-game/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "9137603c628606c954e9",
+    "url": "/clicky-game/static/css/main.df2b074a.chunk.css"
+  },
+  {
+    "revision": "51017a42c76db6aa2275d000a2159054",
+    "url": "/clicky-game/index.html"
+  }
+];
